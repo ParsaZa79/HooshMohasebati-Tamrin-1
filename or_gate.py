9f47246da2f77d297cfa26873567a1af26783e9b
Parsa Zeinali <pzeinali4@gmail.com>
2025-04-18 18:48:51 +0330

    Add or_gate.py to implement the OR gate functionality using the Perceptron model. Includes a main function to run the OR gate simulation, expanding the logical gates project.

---
+++ or_gate.py
@@ -0,0 +1,7 @@
+from gates_part import PerceptronManual, plot_decision_regions_manual, plot_errors, run_or_gate
+
+def run_or_gate_section():
+    return run_or_gate()
+
+if __name__ == "__main__":
+    run_or_gate_section() 
