afa44453143d00339eb5af4a97f92f2ec5a22539
Parsa Zeinali <pzeinali4@gmail.com>
2025-04-18 18:48:42 +0330

    Add and_gate.py to implement the AND gate functionality using the Perceptron model. Includes a main function to run the AND gate simulation, enhancing the logical gates project.

---
+++ and_gate.py
@@ -0,0 +1,7 @@
+from gates_part import PerceptronManual, plot_decision_regions_manual, plot_errors, run_and_gate
+
+def run_and_gate_section():
+    return run_and_gate()
+
+if __name__ == "__main__":
+    run_and_gate_section() 
