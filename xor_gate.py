ffb407f00640b90096b8268312ebed5b60d996ac
Parsa Zeinali <pzeinali4@gmail.com>
2025-04-18 18:48:58 +0330

    Add xor_gate.py to implement the XOR gate functionality using the Perceptron model. Includes a main function to run the XOR gate simulation, further expanding the logical gates project.

---
+++ xor_gate.py
@@ -0,0 +1,7 @@
+from gates_part import PerceptronManual, plot_decision_regions_manual, plot_errors, run_xor_gate
+
+def run_xor_gate_section():
+    return run_xor_gate()
+
+if __name__ == "__main__":
+    run_xor_gate_section() 
